a4b3dca58860ca2ef10b31673d2e33371a3e2061
Alexandre Perez <alexandre.perez92@gmail.com>
2014-01-04 15:39:20 +0100

    Test and correct last tiny things

--- recInterpreter.py
+++ recInterpreter.py
@@ -1,8 +1,10 @@
-import AST, copy
+import AST
+import copy
 from svgwrite import text, shapes, drawing
 from AST import addToClass
 from functools import reduce
 from lex import reserved_words
+
 
 class Transformation:
     def __init__(self, type):
@@ -21,30 +23,32 @@ class Transformation:
 
 
 operator = {
-    '+' : lambda x,y: x+y,
-    '-' : lambda x,y: x-y,
-    '*' : lambda x,y: x*y,
-    '/' : lambda x,y: x/y,
+    '+': lambda x, y: x+y,
+    '-': lambda x, y: x-y,
+    '*': lambda x, y: x*y,
+    '/': lambda x, y: x/y,
 }
 
 conditional_operator = {
-    '==' : lambda x,y: x==y,
-    '<=' : lambda x,y: x<=y,
-    '>=' : lambda x,y: x>=y,
-    '<' : lambda x,y: x<y,
-    '>' : lambda x,y: x>y,
+    '==': lambda x, y: x == y,
+    '<=': lambda x, y: x <= y,
+    '>=': lambda x, y: x >= y,
+    '<': lambda x, y: x < y,
+    '>': lambda x, y: x > y,
 }
 
-vars ={}
+vars = {}
 dwg = None
 step = 0
 
-@addToClass(AST.ProgramNode)# Done
+
+@addToClass(AST.ProgramNode)
 def execute(self):
     for c in self.children:
         c.execute()
 
-@addToClass(AST.TokenNode)# Done
+
+@addToClass(AST.TokenNode)
 def execute(self):
     if isinstance(self.tok, str) and not self.real_string:
         if self.tok.upper() == 'STEP':
@@ -58,33 +62,39 @@ def execute(self):
                 print("Error: variable %s undefined!" % self.tok)
     return self.tok
 
-@addToClass(AST.OpNode)# Done
+
+@addToClass(AST.OpNode)
 def execute(self):
     args = [c.execute() for c in self.children]
     if len(args) == 1:
         args.insert(0,0)
     return reduce(operator[self.op], args)
 
-@addToClass(AST.AssignNode)# Done
+
+@addToClass(AST.AssignNode)
 def execute(self):
     vars[self.children[0].tok] = self.children[1].execute()
 
-@addToClass(AST.WhileNode)# Done
+
+@addToClass(AST.WhileNode)
 def execute(self):
     while self.children[0].execute():
         self.children[1].execute()
 
-@addToClass(AST.IfNode)# Done
+
+@addToClass(AST.IfNode)
 def execute(self):
     if self.children[0].execute():
         self.children[1].execute()
 
-@addToClass(AST.ForNode)#TODO step
+
+@addToClass(AST.ForNode)
 def execute(self):
     for x in range(self.children[0].execute(), self.children[1].execute()):
         self.children[2].execute()
         global step
         step = x
+
 
 @addToClass(AST.ApplyNode)
 def execute(self):
@@ -109,6 +119,7 @@ def execute(self):
         args = self.children[0].execute()
     return [self.key, args]
 
+
 @addToClass(AST.ArgumentsNode)
 def execute(self):
     arguments = {}
@@ -121,6 +132,7 @@ def execute(self):
         else:
             arguments[argument.execute()[0]] = argument.execute()[1]
     return arguments
+
 
 @addToClass(AST.ShapeNode)
 def execute(self):
@@ -166,10 +178,12 @@ def execute(self):
     shape.update(arguments)
     return shape
 
+
 @addToClass(AST.PointNode)
 def execute(self):
     arguments = self.children[0].execute()
     return {'x': arguments["x"], 'y': arguments["y"]}
+
 
 @addToClass(AST.ColorNode)
 def execute(self):
@@ -182,11 +196,13 @@ def execute(self):
     elif type == 'RGB':
         return "rgb(%d,%d,%d)" % (int(arguments[1][0].execute()), int(arguments[1][1].execute()), int(arguments[1][2].execute()))
 
+
 @addToClass(AST.TransformationNode)
 def execute(self):
     transformation = Transformation(self.children[0].tok)
     transformation.add_arguments(self.children[1].execute())
     return transformation
+
 
 @addToClass(AST.ApplyBodyNode)
 def execute(self):
@@ -194,6 +210,7 @@ def execute(self):
     for shape in self.children:
         shapes.append(shape.execute())
     return shapes
+
 
 @addToClass(AST.ConditionalNode)
 def execute(self):
@@ -205,11 +222,11 @@ def execute(self):
 
 if __name__ == "__main__":
     from parse import parse
-    import sys
 
     path = 'Tests/'
     ext = '.pnp'
     files = ['clock', 'comboTest1', 'comboTest2', 'comboTest3', 'customShapeTest', 'helloTest', 'ifTest', 'loopTest', 'loopTest2', 'rotationTest', 'rotationTest2', 'simpleShapesTest', 'someTransforms']
+    files = ['comboTest3']
 
     for file in files:
         print('--------------------------------------')
@@ -217,7 +234,7 @@ if __name__ == "__main__":
         print('--------------------------------------')
         fullpath = path + file + ext
         prog = open(fullpath).read()
-        vars ={}
+        vars = {}
         dwg = drawing.Drawing(filename=file+'.svg', debug=False, profile='tiny')
         ast = parse(prog)
         ast.execute()
